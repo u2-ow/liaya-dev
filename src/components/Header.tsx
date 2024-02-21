33643ef5f08876cc27b9b4188e1271696e50dea6
u2-ow <utwo4805@gmail.com>
2024-02-22 00:19:01 +0900

    edit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,10 @@ export const Header=()=>{
     return(
         <header className={Styles.header}>
             <div className={Styles.headerInner}>
-                <h1 className={Styles.headerInnerH1}>u2.dev</h1>
+                <h1 className={Styles.headerInnerH1}><a href="/">u2.dev</a></h1>
+                <div className={Styles.headerInnerMode}>
+                    <img src="../../assets/light.svg" alt="" width="20px" height="20" />
+                </div>
             </div>
         </header>
     )
--- src/components/Mv.tsx
+++ src/components/Mv.tsx
@@ -6,7 +6,8 @@ export const MV =()=>{
             <div className={Styles.mv}>
                 <div className={Styles.mvInner}>
                     <p className={Styles.mvInnerTitle}>Welcome to <span className={Styles.mvInnerTitleSpan}>U2.dev</span> 🥳</p>
-                    <p className={Styles.mvInnerText}>週に1投稿を目標で運営をしていきます。自分の興味の内容を投稿します。</p>
+                    <p className={Styles.mvInnerText}>週に1投稿を目標で運営をしています。自分の興味の内容を投稿します。</p>
+                    <a href="/" className={Styles.mvInnerLink}>about me <i class="fa-solid fa-angles-right mvInnerLinkArrow" className={Styles.mvInnerLinkArrow}></i></a>
                 </div>
             </div>
   
