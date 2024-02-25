df57bb080a44de6e98613af047edd8df9df46601
u2-ow <utwo4805@gmail.com>
2024-02-26 00:24:42 +0900

    edit component

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -7,14 +7,6 @@ type Props = {
   }
 
 export const SectionTitle =({text}:Props)=>{
-    // const [mode, setMode] = useState(null);
-    // const osDark = matchMedia("(prefers-color-scheme: dark)");
-    
-    // useEffect(() => {
-    //   const osDark = matchMedia("(prefers-color-scheme: dark)");
-    //   const osDarkState = osDark.matches;
-    //   setMode(osDarkState);
-    // });
     return(
         <>
          <h2 className={Styles.sectionTitle}>{text}</h2>
