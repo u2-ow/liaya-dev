f386d555fe6ecd4aaaa92d888fe17580199b18b0
u2-ow <utwo4805@gmail.com>
2024-02-29 18:29:02 +0900

    edit component

--- src/components/ArticlePostArea.tsx
+++ src/components/ArticlePostArea.tsx
@@ -1,13 +1,12 @@
 import Styles from "../styles/articlePostArea.module.scss"
 import dayjs from "dayjs";
 import { SectionTitle } from "./SectionTitle"
-
 export const ArticlePostArea =({blog})=>{
 
     return(
         <>
          <div className={Styles.articlePostArea}>
-         <SectionTitle text="記事一覧" client:load />
+         <SectionTitle text="記事一覧"/>
          <ul className={Styles.articlePosts}>
          {blog.contents && Array.isArray(blog.contents) && blog.contents.map((item, index) => (
             <li key={index} className={Styles.articlePostsItem}>
@@ -15,14 +14,12 @@ export const ArticlePostArea =({blog})=>{
                     <img src={item.eyecatch?.url} alt="" className={Styles.articlePostsItemThm} />
                     <p className={Styles.articlePostsItemTitle}>{item.title}</p>
                     <p className={Styles.articlePostsItemDate}>{dayjs(item.publishedAt).format('YYYY/MM/DD')}</p>
-                    <p className={Styles.articlePostsItemDesc}>{item?.description}</p>
+                
                 </a>
             </li>
         ))}
          </ul>
-
          </div>
-  
         </>
     )
 }
