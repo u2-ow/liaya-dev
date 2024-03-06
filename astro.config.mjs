89fba27cee2c864826140bda3af88279702d3724
u2-ow <utwo4805@gmail.com>
2024-03-07 00:36:19 +0900

    update 色々直した

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/dist",
   integrations: [
     react({
       experimentalReactChildren: true,
